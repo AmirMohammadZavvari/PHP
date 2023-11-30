2a300249a8698e5f1e2f82ef486fb88ebdf50ccd
AmirMohammadZavvari <amiraliphic@gmail.com>
2023-11-30 14:09:01 +0330

    include_veblog added

---
+++ include_veblog/include.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <script src="https://kit.fontawesome.com/134ae673d2.js" crossorigin="anonymous"></script>
+    <title>AmirMohammadZavvari</title>
+    <link rel="stylesheet" href="../tamrin8_and_9/css/style8v9.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Epilogue:wght@300&family=Pacifico&display=swap" rel="stylesheet">
+    <!-- Latest compiled and minified CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Latest compiled JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+    <header>
+    <?php
+        include 'header.html';
+    ?>
+    </header>
+    <div class="MY_BUTTON py-1 m-5 rounded-pill"></div>
+    <footer>
+    <?php
+        include 'footer.html';
+    ?>
+    </footer>
+</body>
+</html>
+
+
